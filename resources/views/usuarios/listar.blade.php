4384f3349837ba9d8c28db29763e13b351174c9d
FreddyCruzM <freddy.cruz2464@gmail.com>
2024-01-23 07:57:06 -0600

    Update Version 1

--- resources/views/usuarios/listar.blade.php
+++ resources/views/usuarios/listar.blade.php
@@ -31,6 +31,11 @@
                         <td>{{ $user->nombre }}</td>
                         <td>{{ $user->email }}</td>
                         <td>
+
+                        <a href="{{ route('editform', $user->id) }}" Class="btn btn-primary mb-1" >
+                            <i class="fas fa-pencil-alt"> </i>
+                        </a>
+
                         <form action="{{ route('delete', $user->id)}}" method="POST">
                             @csrf @method('DELETE')
 
